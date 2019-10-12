90a1bd4e91f2fb60d841eb5fb7cabe4fd9e2c7da
AAimair <981426145@qq.com>
2019-10-12 14:04:43 +0800

    qq

--- src/router.js
+++ src/router.js
@@ -1,28 +1,43 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
-Vue.use(ElementUI)
-Vue.use(Router)
+Vue.use(ElementUI);
+Vue.use(Router);
 
 export default new Router({
   // mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      redirect:'/Melliodas',
-      name: 'home',
+      path: "/",
+      redirect: "/Melliodas",
+      name: "home",
       component: Home,
-      children:[
-
-        {path: '/sin',name: 'sin',component: () => import('./views/sin.vue')},
-        {path: '/Melliodas',name: 'Melliodas',component: () => import('./views/Melliodas.vue')},
-        {path: '/Elizabeth',name: 'Elizabeth',component: () => import('./views/Elizabeth.vue')},
-        {path: '/Diana',name: 'Diana',component: () => import('./views/Diana.vue')}
+      children: [
+        {
+          path: "/sin",
+          name: "sin",
+          component: () => import("./views/sin.vue")
+        },
+        {
+          path: "/Melliodas",
+          name: "Melliodas",
+          component: () => import("./views/Melliodas.vue")
+        },
+        {
+          path: "/Elizabeth",
+          name: "Elizabeth",
+          component: () => import("./views/Elizabeth.vue")
+        },
+        {
+          path: "/Diana",
+          name: "Diana",
+          component: () => import("./views/Diana.vue")
+        }
       ]
-    },
+    }
   ]
-})
+});
